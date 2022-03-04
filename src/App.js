aa45bbb21fa70c5ea4deb44cb9c634dc52bcd2d1
Thuta <thutaminthway2001@gmail.com>
2022-03-04 22:39:57 +0630

    adding product and productlist

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import About from "./PortfolioContainer/About/About";
 import Intro from "./PortfolioContainer/Intro/Intro";
+import ProductList from "./PortfolioContainer/ProductList/ProductList";
 
 const App = (props) => {
   return (
     <div className="App">
       <Intro />
       <About />
+      <ProductList />
     </div>
   );
 };
---
+++ src/data.js
@@ -0,0 +1,32 @@
+export const products = [
+  {
+    id: 1,
+    img: "https://images.pexels.com/photos/11141607/pexels-photo-11141607.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+    link: "http://lama.dev",
+  },
+  {
+    id: 2,
+    img: "https://images.pexels.com/photos/11141607/pexels-photo-11141607.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+    link: "http://lama.dev",
+  },
+  {
+    id: 3,
+    img: "https://images.pexels.com/photos/11141607/pexels-photo-11141607.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+    link: "http://lama.dev",
+  },
+  {
+    id: 4,
+    img: "https://images.pexels.com/photos/11141607/pexels-photo-11141607.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+    link: "http://lama.dev",
+  },
+  {
+    id: 5,
+    img: "https://images.pexels.com/photos/11141607/pexels-photo-11141607.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+    link: "http://lama.dev",
+  },
+  {
+    id: 6,
+    img: "https://images.pexels.com/photos/11141607/pexels-photo-11141607.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+    link: "http://lama.dev",
+  },
+];
